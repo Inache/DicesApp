33da0829aa5c5475873ae9836584ea1fce46c73c
Inache <evgeniy.a.osminin@gmail.com>
2018-02-24 14:18:27 +0200

    Started to do java

--- app/src/main/java/com/inache/dicesapp/MainActivity.java
+++ app/src/main/java/com/inache/dicesapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.inache.dicesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button rollButton = findViewById(R.id.rollButton);
+        ImageView image_left_dice = findViewById(R.id.image_left_dice);
+        ImageView image_right_dice = findViewById(R.id.image_right_dice);
+
+
     }
 }
